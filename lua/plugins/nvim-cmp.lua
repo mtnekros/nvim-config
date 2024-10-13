2287bcf4473262242168e332dda61013e7ad5198
Diwash Tamang <mtnekros@gmail.com>
2024-10-13 08:14:13 +0545

    [feat]: add custom blog outline snippet with luasnip

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -15,15 +15,13 @@ return { -- Autocompletion
 				return "make install_jsregexp"
 			end)(),
 			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets:
-				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   'rafamadriz/friendly-snippets',
-				--   config = function()
-				--     require('luasnip.loaders.from_vscode').lazy_load()
-				--   end,
-				-- },
+                -- for vscode style snippets
+				{
+				  'rafamadriz/friendly-snippets',
+				  config = function()
+				    require('luasnip.loaders.from_vscode').lazy_load()
+				  end,
+				},
 			},
 		},
 		"saadparwaiz1/cmp_luasnip",
@@ -46,16 +44,10 @@ return { -- Autocompletion
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			completion = { completeopt = "menu,menuone,noinsert" },
+			completion = { completeopt = "menu,menuone,noinsert,noselect" },
 
-			-- For an understanding of why these mappings were
-			-- chosen, you will need to read `:help ins-completion`
-			--
-			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
 				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
 				["<C-p>"] = cmp.mapping.select_prev_item(),
 
 				-- Scroll the documentation window [b]ack / [f]orward
@@ -67,25 +59,12 @@ return { -- Autocompletion
 				--  This will expand snippets if the LSP sent a snippet.
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
-				-- If you prefer more traditional completion keymaps,
-				-- you can uncomment the following lines
-				--['<CR>'] = cmp.mapping.confirm { select = true },
-				--['<Tab>'] = cmp.mapping.select_next_item(),
-				--['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
 				-- Manually trigger a completion from nvim-cmp.
 				--  Generally you don't need this, because nvim-cmp will display
 				--  completions whenever it has completion options available.
 				["<C-Space>"] = cmp.mapping.complete({}),
 
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
+                -- move through the snippet customizable <++> arguments
 				["<C-l>"] = cmp.mapping(function()
 					if luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
@@ -96,10 +75,21 @@ return { -- Autocompletion
 						luasnip.jump(-1)
 					end
 				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-			}),
+                ["<C-j>"] = cmp.mapping(function()
+                    if luasnip.choice_active() then
+                        luasnip.change_choice(1)
+                    else
+                        print("No active choice")
+                    end
+                end, {"i", "s"}),
+                ["<C-k>"] = cmp.mapping(function()
+                    if luasnip.choice_active() then
+                        luasnip.change_choice(-1)
+                    else
+                        print("No active choice")
+                    end
+                end, {"i", "s"}),
+                }),
 			sources = {
 				{
 					name = "lazydev",
@@ -111,5 +101,10 @@ return { -- Autocompletion
 				{ name = "path" },
 			},
 		})
+
+        -- add custom snippets
+        require("plugins.custom-snippets")
 	end,
 }
+
+
