fd12b82d18e4f51fb657cc04cb5b1d0070cd76e7
Diwash Tamang <mtnekros@gmail.com>
2024-10-14 19:41:20 +0545

    [feat] add snippet for recursive task items

--- lua/custom/snippets/md-snippets.lua
+++ lua/custom/snippets/md-snippets.lua
@@ -16,6 +16,26 @@ local blog_outline_template = [[
 <Add a question here>
 ]]
 
+local function add_tasks_recursively()
+    return ls.snippet_node(
+        nil,
+        ls.choice_node(1, {
+            ls.text_node(""),
+            ls.snippet_node(nil, {
+                ls.text_node({"", "* ["}), -- for new line
+                ls.choice_node(1, {
+                    ls.text_node(" "),
+                    ls.text_node("X"),
+                    ls.text_node("-")
+                }),
+                ls.text_node("] "),
+                ls.insert_node(2, "task"),
+                ls.dynamic_node(3, add_tasks_recursively),
+           })
+        })
+    )
+end
+
 local function add_subtitle_recursively()
     return ls.snippet_node(
         nil,
@@ -31,8 +51,8 @@ local function add_subtitle_recursively()
 end
 
 ls.add_snippets('markdown', {
-    ls.snippet(
-        'bol',
+    -- blog outline snippet
+    ls.snippet('bol',
         fmt(blog_outline_template, {
             title = ls.insert_node(1, "title"),
             overview = ls.insert_node(2, "<add an hook here and/or the overview of the blog>"),
@@ -41,5 +61,13 @@ ls.add_snippets('markdown', {
             conclusion = ls.insert_node(5, "<add conclusion here>"),
         })
     ),
+    -- tasks snippet
+    ls.snippet('tasks',
+        fmt("* [{check}] {task}{more_task}", {
+            check = ls.choice_node(1, {ls.text_node(" "), ls.text_node("X"), ls.text_node("-")}),
+            task  = ls.insert_node(2, "task"),
+            more_task = ls.dynamic_node(3, add_tasks_recursively),
+        })
+   ),
 })
 
