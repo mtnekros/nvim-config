d079dc31e2bd618460a0ec796aa8039ec44ca358
Diwash Tamang <mtnekros@gmail.com>
2024-10-16 11:27:45 +0545

    [feat]: don't lazy load vim-fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,5 +1,6 @@
 return {
 	"tpope/vim-fugitive",
+    lazy = false,
 	keys = {
 		{ "<leader>gs", ":Git<CR>", desc = "[G]it [S]tatus" },
 		{ "<leader>gj", ":diffget //3<CR> ", desc = "[G]it [R]ight" },
