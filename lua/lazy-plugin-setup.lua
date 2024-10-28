93b49eadcee21d6b1c9e8e3106398985581ddf20
Diwash Tamang <i34380@verisk.com>
2024-10-28 13:56:48 +0545

    [feat]: add vim polyglot for syntax (especially for simple Markdown SHL)

--- lua/lazy-plugin-setup.lua
+++ lua/lazy-plugin-setup.lua
@@ -11,6 +11,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	-- "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+    'sheerun/vim-polyglot',
 	require("plugins.which-key"),
 	require("plugins.telescope"),
 	-- LSP Plugins
