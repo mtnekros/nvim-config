eb40fc8d974b2a5f6ff201da62982b669ee41084
Diwash Tamang <i34380@verisk.com>
2025-06-04 00:40:29 +0545

    feat: no tab complete for copilot

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,4 +1,12 @@
+
 return { -- Autoformat
-	"github/copilot.vim",
+    "github/copilot.vim",
+    config = function()
+        vim.keymap.set('i', '<C-Y>', 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false
+        })
+        vim.g.copilot_no_tab_map = true
+    end,
 }
 
